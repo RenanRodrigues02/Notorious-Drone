f113cc8a3672b8dee59db1b9cc08096f9760be72
RenanRodrigues02 <devrenan02@gmail.com>
2022-02-25 16:19:58 -0300

    funcao galeria-drones concluída

--- js/script.js
+++ js/script.js
@@ -23,3 +23,42 @@ function ativarProduto (parametro) {
     }
 }
 parametros.forEach(ativarProduto)
+
+//Perguntas Frquentes
+
+const perguntas= document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains ("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//Galeria de Drones
+
+const galeria = document.querySelectorAll(".drone-imagens img");
+const galeriaContainer = document.querySelector (".drone-imagens");
+
+function trocarImagem(event){
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+    if(media) {
+        galeriaContainer.prepend(img);
+    }
+    
+}
+
+function eventosGaleria (img){
+    img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
